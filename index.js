d1f67f721b060a10b5f5afc9db4ffc18ffd1cf52
gurunganjila27@gmail.com <gurunganjila27@gmail.com>
2024-06-27 21:58:37 +0545

    updated readme

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 //----day2-------
 //ES5
-// person ="Anjila";
-//console.log(person);
+person = "Anjila";
+console.log(person);
 
 //ES6
 //let
 //const
-//alert("Hi");
+alert("Hi");
 
-// const age = prompt("How old are you?");
-// console.log(Number(age));
+const age = prompt("How old are you?");
+console.log(Number(age));
 
-// const isvalid = confirm("Are you above 18?");
-// console.log(isvalid);
+const isvalid = confirm("Are you above 18?");
+console.log(isvalid);
 
 //write a js program that ask user for principle, interest rate and time..
 //alert the interest amount
 
-// const p = prompt("Enter principle");
-// const r = prompt("Enter rate");
-// const t = prompt("Enter time");
+const p = prompt("Enter principle");
+const r = prompt("Enter rate");
+const t = prompt("Enter time");
 
-// console.log(Number(p));
-// console.log(Number(r));
-// console.log(Number(t));
-// i = (p*t*r)/100;
-// alert(`The interest is ${i}`);
+console.log(Number(p));
+console.log(Number(r));
+console.log(Number(t));
+i = (p * t * r) / 100;
+alert(`The interest is ${i}`);
 
-//type conversion or type coercion
-// const num = string(10);
-// const num1 = Number(num);
+//-----type conversion or type coercion-----
+const num = string(10);
+const num1 = Number(num);
 
-//statements
+//----statements----
 // write a js program that ask for their gender(m/f)
 //use if else and alert user about theiir gender
 
-// let gender = prompt("Tell me your gender");
-// gender === "m"
-//   ? alert(`You are male`)
-//   : gender === "f"
-//   ? alert(`You are female`)
-//   : alert(`You are trans`);
+let gender = prompt("Tell me your gender");
+gender === "m"
+  ? alert(`You are male`)
+  : gender === "f"
+  ? alert(`You are female`)
+  : alert(`You are trans`);
 
 //-----switch case-----
 //write a js program to calculate grade
-// const marks = prompt("Enter your marks");
-// switch (true) {
-//   case marks > 80:
-//     alert("Grade A");
-//     break;
-//   case marks > 60 && marks < 80:
-//     alert("Grade B");
-//     break;
-//   case marks < 60:
-//     alert("Grade C");
-//     break;
-//   default:
-//     alert("Invalid number");
-// }
+const marks = prompt("Enter your marks");
+switch (true) {
+  case marks > 80:
+    alert("Grade A");
+    break;
+  case marks > 60 && marks < 80:
+    alert("Grade B");
+    break;
+  case marks < 60:
+    alert("Grade C");
+    break;
+  default:
+    alert("Invalid number");
+}
 
 //------operator-----
 //write a js to ask user for username and password
@@ -65,26 +65,27 @@
 //alert welcome 'your name'
 //alert invalid credential
 
-// const username = prompt("Enter your username");
-// const password = prompt("Enter your password");
-// // if (username === "Anji" || password === "Anji") {
-// //   alert("Welcome Anji");
-// // } else {
-// //   alert("Invalid credentials");
-// // }
-// username === "Anji" && password === "Anji"
-//   ? alert("Welcome Anji")
-//   : alert("Invalid credentials");
+const username = prompt("Enter your username");
+const password = prompt("Enter your password");
+// if (username === "Anji" || password === "Anji") {
+//   alert("Welcome Anji");
+// } else {
+//   alert("Invalid credentials");
+// }
+username === "Anji" && password === "Anji"
+  ? alert("Welcome Anji")
+  : alert("Invalid credentials");
 
 //--loop-----
 //multiplication table of 13
-//let i = 1;
-// while (i <= 10) {
-//   mul = 13 * i;
-//   console.log(mul);
-//   i++;
-// }
+let i = 1;
+while (i <= 10) {
+  mul = 13 * i;
+  console.log(mul);
+  i++;
+}
 
+// convert anjila and gurung to Anjila Gurung
 const fName = "anjila";
 const lName = "gurung";
 //output: "Anjila Gurung"
